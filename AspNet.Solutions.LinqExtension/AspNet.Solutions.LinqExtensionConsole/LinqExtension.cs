d97553acbb5208b9345fe00796e16647f3526712
cagdaskarademir <cagdaskarademir@gmail.com>
2015-06-01 16:24:33 +0300

    completed

---
+++ AspNet.Solutions.LinqExtension/AspNet.Solutions.LinqExtensionConsole/LinqExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AspNet.Solutions.LinqExtensionConsole
+{
+    public static class LinqExtension
+    {
+        public static T GetNth<T>(this IEnumerable<T> list, int n)
+        {
+            return list.Where((data, i) => i == n).FirstOrDefault();
+        }
+
+        public static T GetNth<T>(this IList<T> list, int n)
+        {
+            return list.Where((data, i) => i == n).FirstOrDefault();
+        }
+    }
+}
---
+++ AspNet.Solutions.LinqExtension/AspNet.Solutions.LinqExtensionConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNet.Solutions.LinqExtensionConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = new List<string>()
+            {
+                "demo1",
+                "demo2",
+                "demo3",
+                "demo4",
+                "demo5"
+            };
+
+            Console.WriteLine(list.GetNth(4));
+
+            Console.ReadLine();
+
+        }
+    }
+}
